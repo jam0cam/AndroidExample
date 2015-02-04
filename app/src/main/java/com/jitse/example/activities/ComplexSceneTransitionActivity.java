1ba55c1f8fc03529f885efd809bbe5a5260a5ba6
Jia Tse <jitse@zappos.com>
2015-02-04 14:30:51 -0800

    added complex transition example

--- app/src/main/java/com/jitse/example/activities/ComplexSceneTransitionActivity.java
+++ app/src/main/java/com/jitse/example/activities/ComplexSceneTransitionActivity.java
@@ -5,19 +5,21 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.jitse.example.R;
+import com.squareup.picasso.Picasso;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
-public class SceneTransitionActivity extends ActionBarActivity {
+public class ComplexSceneTransitionActivity extends ActionBarActivity {
 
-    private static final String TAG = SceneTransitionActivity.class.getName();
+    private static final String TAG = ComplexSceneTransitionActivity.class.getName();
     @InjectView(R.id.button)
     Button mButton;
 
@@ -31,29 +33,39 @@ public class SceneTransitionActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scene_transition);
+        setContentView(R.layout.activity_complex_scene_transition);
         ButterKnife.inject(this);
 
-        String something = getIntent().getStringExtra("something");
+        String url = getIntent().getStringExtra("something");
 
-        if ("hello".equals(something)) {
-            mImageView.setImageDrawable(getResources().getDrawable(R.drawable.adidas));
+        if (!TextUtils.isEmpty(url)) {
+            Picasso.with(this)
+                    .load("http://a1.zassets.com/images/z/3/0/2/7/2/8/3027286-p-MULTIVIEW.jpg")
+                    .into(mImageView);
             mThumbnail.setImageBitmap(null);
+        } else {
+            initThumbnail();
         }
 
         Log.d(TAG, "onCreate Finish");
     }
 
+    public void initThumbnail() {
+        Log.d(TAG, "loadin from picasso");
+        Picasso.with(this)
+                .load("http://a1.zassets.com/images/z/3/0/2/7/2/8/3027286-p-MULTIVIEW.jpg")
+                .into(mThumbnail);
+    }
+
     @OnClick(R.id.button)
     public void clicked(){
         Log.d(TAG, "onClicked");
-        Intent intent = new Intent(this, SceneTransitionActivity.class);
+        Intent intent = new Intent(this, ComplexSceneTransitionActivity.class);
 
-        intent.putExtra("something","hello");
+        intent.putExtra("something", "http://a1.zassets.com/images/z/3/0/2/7/2/8/3027286-p-MULTIVIEW.jpg");
 
         ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, mThumbnail, "image");
         ActivityCompat.startActivity(this, intent, options.toBundle());
         Log.d(TAG, "onClick finished");
     }
-
 }
