a2a03289cc4aa83b853588eab92f66b922b53b70
Jia Tse <jitse@zappos.com>
2015-08-21 09:56:21 -0700

    added NFC example to launch the app

---
+++ app/src/main/java/com/jitse/example/activities/NfcActivity.java
@@ -0,0 +1,53 @@
+package com.jitse.example.activities;
+
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.jitse.example.R;
+
+public class NfcActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback {
+
+    private static final String TAG = NfcActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        Log.d(TAG, "beaming message");
+        String text = ("Beam me up, Android!\n\n" +
+                "Beam Time: " + System.currentTimeMillis());
+        NdefMessage msg = new NdefMessage(
+                new NdefRecord[] {  NdefRecord.createMime(
+                        "application/vnd.com.jitse.example", text.getBytes())
+                        /**
+                         * The Android Application Record (AAR) is commented out. When a device
+                         * receives a push with an AAR in it, the application specified in the AAR
+                         * is guaranteed to run. The AAR overrides the tag dispatch system.
+                         * You can add it back in to guarantee that this
+                         * activity starts when receiving a beamed message. For now, this code
+                         * uses the tag dispatch system.
+                         */
+                        , NdefRecord.createApplicationRecord("com.jitse.example")
+                });
+        return msg;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Log.d(TAG, "activity started from android beam");
+        }
+    }
+
+}
