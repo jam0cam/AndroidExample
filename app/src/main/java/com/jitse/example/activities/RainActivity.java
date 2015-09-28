47be496a5346cb4b05e0a1e6d7cc557770b76778
Jia Tse <jitse@zappos.com>
2015-09-28 15:06:49 -0700

    added shake gesture to activate bat animation

--- app/src/main/java/com/jitse/example/activities/RainActivity.java
+++ app/src/main/java/com/jitse/example/activities/RainActivity.java
@@ -1,18 +1,29 @@
 package com.jitse.example.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.widget.Button;
 
 import com.jitse.example.R;
+import com.jitse.example.ShakeDetector;
 import com.plattysoft.leonids.ParticleSystem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
 public class RainActivity extends Activity {
+
+    private static final String TAG = RainActivity.class.getName();
 
     @InjectView(R.id.btn_red_dots)
     Button mBtnOneShot;
@@ -29,17 +40,25 @@ public class RainActivity extends Activity {
     @InjectView(R.id.btn_emit_shoes)
     Button mBtnEmitShoes;
 
-
     ParticleSystem bean1;
     ParticleSystem bean2;
 
     ParticleSystem snow1;
     ParticleSystem snow2;
+
+    List<ParticleSystem> batParticles;
+
     boolean beanToggle = false;
     boolean flakeToggle = false;
+    boolean batToggle = false;
 
     String santaUrl = "http://www.carlswebgraphics.com/christmas/3-animated-santa-1.gif";
     private AnimationDrawable mSantaAnimation;
+
+    // The following are used for the shake detection
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +66,38 @@ public class RainActivity extends Activity {
         setContentView(R.layout.activity_rain);
 
         ButterKnife.inject(this);
+
+
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                Log.d(TAG, "Shaking");
+                if (!batToggle) {
+                    toggleBats();
+                }
+            }
+        });
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
 
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
 
     @OnClick(R.id.btn_red_dots)
     public void startRain() {
@@ -116,6 +164,7 @@ public class RainActivity extends Activity {
 
         flakeToggle = !flakeToggle;
     }
+
     @OnClick(R.id.btn_emit_shoes)
     public void shoeExplosion() {
         new ParticleSystem(this, 1, R.drawable.bk, 10000)
@@ -143,27 +192,67 @@ public class RainActivity extends Activity {
                 .oneShot(mBtnEmitShoes, 1);
 
         new ParticleSystem(this, 1, R.drawable.bk, 10000)
-                .setSpeedModuleAndAngleRange(0.2f, 0.1f, 180, 180)
+                .setSpeedModuleAndAngleRange(0.1f, 0.1f, 180, 180)
                 .setRotationSpeed(150)
                 .setAcceleration(0.00035f, 90)
                 .oneShot(mBtnEmitShoes, 1);
 
         new ParticleSystem(this, 1, R.drawable.baker, 10000)
-                .setSpeedModuleAndAngleRange(0.2f, 0.1f, 225, 225)
+                .setSpeedModuleAndAngleRange(0.1f, 0.1f, 225, 225)
                 .setRotationSpeed(150)
                 .setAcceleration(0.00035f, 90)
                 .oneShot(mBtnEmitShoes, 1);
 
         new ParticleSystem(this, 1, R.drawable.nb1, 10000)
-                .setSpeedModuleAndAngleRange(0.2f, 0.1f, 270, 270)
+                .setSpeedModuleAndAngleRange(0.1f, 0.1f, 270, 270)
                 .setRotationSpeed(150)
                 .setAcceleration(0.00035f, 90)
                 .oneShot(mBtnEmitShoes, 1);
 
         new ParticleSystem(this, 1, R.drawable.nb2, 10000)
-                .setSpeedModuleAndAngleRange(0.2f, 0.1f, 315, 315)
+                .setSpeedModuleAndAngleRange(0.1f, 0.1f, 315, 315)
                 .setRotationSpeed(150)
                 .setAcceleration(0.00035f, 90)
                 .oneShot(mBtnEmitShoes, 1);
+    }
+
+    public void toggleBats() {
+        if (!batToggle) {
+
+            batParticles = new ArrayList<>();
+            createBatParticles(R.id.view_bat_middle);
+            createBatParticles(R.id.view_bat_top);
+            createBatParticles(R.id.view_bat_bottom);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    toggleBats();
+                }
+            }, 2000);
+
+        } else {
+            for (ParticleSystem ps : batParticles) {
+                ps.stopEmitting();
+            }
+        }
+        batToggle = !batToggle;
+    }
+
+    private void createBatParticles(int viewId) {
+        ParticleSystem batMiddle1 = new ParticleSystem(this, 80, R.drawable.bat_animation, 10000);
+        batMiddle1.setSpeedModuleAndAngleRange(0f, 0.1f, 240, 360)
+                .setRotationSpeed(144)
+                .setAcceleration(0.00035f, 0)
+                .emit(findViewById(viewId), 10);
+
+        ParticleSystem batMiddle2 = new ParticleSystem(this, 80, R.drawable.bat_animation, 10000);
+        batMiddle2.setSpeedModuleAndAngleRange(0f, 0.1f, 0, 120)
+                .setRotationSpeed(144)
+                .setAcceleration(0.00035f, 0)
+                .emit(findViewById(viewId), 10);
+
+        batParticles.add(batMiddle1);
+        batParticles.add(batMiddle2);
     }
 }
